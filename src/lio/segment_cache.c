b111299fbdd3523101a5654cbd37720880134cd6
Alan Tackett <alan.tackett@accre.vanderbilt.edu>
2013-04-16 15:59:00 -0500

    Too rushed.  Moved extra code to corrent location

--- src/lio/segment_cache.c
+++ src/lio/segment_cache.c
@@ -2711,13 +2711,13 @@ op_generic_t *segcache_inspect(segment_t *seg, data_attr_t *da, info_fd_t *fd, i
 
   if ((mode != INSPECT_SOFT_ERRORS) && (mode != INSPECT_HARD_ERRORS)) {
      info_printf(fd, 1, XIDT ": Cache segment maps to child " XIDT "\n", segment_id(seg), segment_id(s->child_seg));
-  }
 
-  //** Check the file size first
-  child_size = segment_size(s->child_seg);
-  if (child_size < s->total_size) {
-     info_printf(fd, 1, XIDT ": ERROR Cache segment size(" XOT ") > child segment size(" XOT ")!\n", segment_id(seg), s->total_size, child_size);
-     return(gop_dummy(op_failure_status));
+     //** Check the file size first
+     child_size = segment_size(s->child_seg);
+     if (child_size < s->total_size) {
+        info_printf(fd, 1, XIDT ": ERROR Cache segment size(" XOT ") > child segment size(" XOT ")!\n", segment_id(seg), s->total_size, child_size);
+        return(gop_dummy(op_failure_status));
+     }
   }
 
   return(segment_inspect(s->child_seg, da, fd, mode, bufsize, query, timeout));
