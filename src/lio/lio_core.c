ee330a4f0268202cd424fc2d7198aeae91fcaad2
Alan Tackett <alan.tackett@accre.vanderbilt.edu>
2013-09-26 13:30:20 -0500

    Properly pass the bufsize to lower tasks.  Bug introduced during the cp_* refactor

--- src/lio/lio_core.c
+++ src/lio/lio_core.c
@@ -1565,7 +1565,7 @@ op_status_t cp_local2lio(lio_cp_file_t *cp)
   //** Check if it exists and if not create it
   dtype = lioc_exists(cp->dest_tuple.lc, cp->dest_tuple.creds, cp->dest_tuple.path);
 
-log_printf(5, "src=%s dest=%s dtype=%d\n", cp->src_tuple.path, cp->dest_tuple.path, dtype);
+log_printf(5, "src=%s dest=%s dtype=%d bufsize=" XOT "\n", cp->src_tuple.path, cp->dest_tuple.path, dtype, cp->bufsize);
 
   if (dtype == 0) { //** Need to create it
      err = gop_sync_exec(lio_create_object(cp->dest_tuple.lc, cp->dest_tuple.creds, cp->dest_tuple.path, OS_OBJECT_FILE, NULL, NULL));
@@ -1831,7 +1831,7 @@ op_status_t lio_cp_path_fn(void *arg, int id)
   opque_t *q;
   op_status_t status;
 
-log_printf(15, "START src=%s dest=%s max_spawn=%d\n", cp->src_tuple.path, cp->dest_tuple.path, cp->max_spawn);
+log_printf(15, "START src=%s dest=%s max_spawn=%d bufsize=" XOT "\n", cp->src_tuple.path, cp->dest_tuple.path, cp->max_spawn, cp->bufsize);
 flush_log();
 
   status = op_success_status;
@@ -1865,6 +1865,7 @@ flush_log();
      c = &(cplist[slot]);
      c->src_tuple = cp->src_tuple; c->src_tuple.path = fname;
      c->dest_tuple = cp->dest_tuple; c->dest_tuple.path = strdup(dname);
+     c->bufsize = cp->bufsize;
 
      gop = new_thread_pool_op(lio_gc->tpc_unlimited, NULL, lio_cp_file_fn, (void *)c, NULL, 1);
      gop_set_myid(gop, slot);
