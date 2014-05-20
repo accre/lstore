4cdebd86c71d069af23028b25fa04be01270f779
alok <alok.hota@vanderbilt.edu>
2014-05-20 14:33:42 -0500

    Added streaming. Edited an error check in mqs_test.c. Rigorous bulk tests need to be written now.
    Former-commit-id: 2132b7dadb70c8c4a20d9a59cf6208167396a5f4

---
+++ src/gop/mqs_roundrobin.h
@@ -0,0 +1,21 @@
+#define TEST_SIZE	1024
+#define TEST_DATA	1
+
+
+char *test_data = NULL;
+int test_size = 1024*1024;
+int packet_min = 1024;
+int packet_max = 1024*1024;
+int send_min = 1024;
+int send_max = 10*1024*1024;
+int nparallel = 100;
+int ntotal = 1000;
+//int do_compress = MQS_PACK_RAW;
+int timeout = 10;
+int stream_max_size = 4096;
+int launch_flusher = 0;
+int delay_response = 0;
+int in_process = 0;
+
+int ongoing_server_interval = 5;
+int ongoing_client_interval = 1;
