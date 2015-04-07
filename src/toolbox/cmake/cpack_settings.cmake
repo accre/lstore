8b9d630291f012b1122de34ee6bd5426a5f9f599
Alan Tackett <tacketar@accre.vanderbilt.edu>
2015-04-07 13:25:24 -0500

    Comment cleanup. Left Andrew's RPM lines commented out

--- src/toolbox/cmake/cpack_settings.cmake
+++ src/toolbox/cmake/cpack_settings.cmake
@@ -1,14 +1,9 @@
 #Tweaks for the different package types
-message(STATUS "========loading ${CPACK_GENERATOR} config==========")
 
 if (${CPACK_GENERATOR} STREQUAL "DEB")
-   message(STATUS "Building DEBIAN packages")
    set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON)
-#   set(CPACK_DEB_COMPONENT_INSTALL ON)
-#   set(CPACK_COMPONENTS_ALL libraries headers)
 elseif (${CPACK_GENERATOR} STREQUAL "RPM")
-   message(STATUS "Building RPM packages")
-else()
-   message(STATUS "Building ${CPACK_GENERATOR} packages")
+   #set(CPACK_RPM_PACKAGE_REQUIRES "openssl >= 1.0.1e")
+   #set(CPACK_RPM_PACKAGE_AUTOREQPROV " no")
 endif()
 
