ceb967f7af27442c1d0b5e71f8fd5b5db55f05df
Matt Heller <matthew.f.heller@accre.vanderbilt.edu>
2015-09-24 07:11:07 -0400

    Pulling in the FindAPR module from the lio sub-project. It appears to be more advanced and I fixed it up so that it will be compatible (and also work on my system, Ubuntu-trusty).

--- src/toolbox/cmake/FindAPRUtil-ACCRE.cmake
+++ src/toolbox/cmake/FindAPRUtil-ACCRE.cmake
@@ -1,51 +1,65 @@
-# -*- cmake -*-
+# Locate apr-util include paths and libraries. Based on findapr.cmake;
+# simple modifications to apply to apr-util instead.
 
-# - Find Apache Portable Runtime
-# Find the APR includes and libraries
 # This module defines
-#  APRUTIL_INCLUDE_DIR and APRUTIL_INCLUDE_DIR, where to find apr.h, etc.
-#  APRUTIL_LIBRARIES and APRUTIL_LIBRARIES, the libraries needed to use APR.
-#  APRUTIL_FOUND and APRUTIL_FOUND, If false, do not try to use APR.
-# also defined, but not for general use are
-#  APRUTIL_LIBRARY and APRUTIL_LIBRARY, where to find the APR library.
+# APU_INCLUDES, where to find apu.h, etc.
+# APU_LIBS, linker switches to use with ld to link against apr-util
+# APU_EXTRALIBS, additional libraries to link against
+# APU_LDFLAGS, additional linker flags that must be used
+# APU_FOUND, set to TRUE if found, FALSE otherwise
+# APU_VERSION, set to the version of apr-util found
 
-# APR first.
+set(APU_FOUND FALSE)
 
-# Find the *relative* include path
-find_path(apr_inc NAMES apr-accre-1.0/apr.h apr-ACCRE-1/apr.h)
+find_program(APU_CONFIG_EXECUTABLE apu-ACCRE-1-config)
+mark_as_advanced(APU_CONFIG_EXECUTABLE)
 
-# Now convert it to the full path
-if (apr_inc)     
-   find_path(APR_INCLUDE_DIR NAMES apr.h PATHS ${apr_inc}/apr-accre-1.0 ${apr_inc}/apr-ACCRE-1 )
-else (apr_inc)
-   find_path(APRUTIL_INCLUDE_DIR apr.h)
-endif (apr_inc)
+macro(_apu_invoke _varname _regexp)
+    execute_process(
+        COMMAND ${APU_CONFIG_EXECUTABLE} ${ARGN}
+        OUTPUT_VARIABLE _apu_output
+        RESULT_VARIABLE _apu_failed
+    )
 
-FIND_LIBRARY(APRUTIL_LIBRARY NAMES aprutil-ACCRE-1)
+    if(_apu_failed)
+        message(FATAL_ERROR "${APU_CONFIG_EXECUTABLE} ${ARGN} failed")
+    else()
+        string(REGEX REPLACE "[\r\n]"  "" _apu_output "${_apu_output}")
 
-IF (APRUTIL_LIBRARY AND APRUTIL_INCLUDE_DIR)
-    SET(APRUTIL_LIBRARIES ${APRUTIL_LIBRARY})
-    SET(APRUTIL_FOUND "YES")
-ELSE (APRUTIL_LIBRARY AND APRUTIL_INCLUDE_DIR)
-  SET(APRUTIL_FOUND "NO")
-ENDIF (APRUTIL_LIBRARY AND APRUTIL_INCLUDE_DIR)
+        if(NOT ${_regexp} STREQUAL "")
+            string(REGEX REPLACE "${_regexp}" " " _apu_output "${_apu_output}")
+        endif()
 
+        # trim string after processing the _regexp arg because it may introduce a <space> at the ends of the string
+        string(REGEX REPLACE " +$"     "" _apu_output "${_apu_output}")
+        string(REGEX REPLACE "^ +"     "" _apu_output "${_apu_output}")
 
-IF (APRUTIL_FOUND)
-   IF (NOT APRUTIL_FIND_QUIETLY)
-      MESSAGE(STATUS "Found APRUtil-ACCRE: ${APRUTIL_LIBRARIES}")
-   ENDIF (NOT APRUTIL_FIND_QUIETLY)
-ELSE (APRUTIL_FOUND)
-   IF (APRUTIL_FIND_REQUIRED)
-      MESSAGE(FATAL_ERROR "Could not find APRUtil-ACCRE library")
-   ENDIF (APRUTIL_FIND_REQUIRED)
-ENDIF (APRUTIL_FOUND)
+        # XXX: We don't want to invoke separate_arguments() for APU_LDFLAGS;
+        # just leave as-is
+        if(NOT ${_varname} STREQUAL "APU_LDFLAGS")
+            separate_arguments(_apu_output)
+        endif()
 
-# Deprecated declarations.
-SET (NATIVE_APRUTIL_INCLUDE_PATH ${APRUTIL_INCLUDE_DIR} )
-GET_FILENAME_COMPONENT (NATIVE_APRUTIL_LIB_PATH ${APRUTIL_LIBRARY} PATH)
+        set(${_varname} "${_apu_output}")
+    endif()
+endmacro(_apu_invoke)
 
-MARK_AS_ADVANCED(
-  APRUTIL_LIBRARY
-  APRUTIL_INCLUDE_DIR
-  )
+_apu_invoke(APU_INCLUDES  "(^| )-I" --includes)
+_apu_invoke(APU_EXTRALIBS "(^| )-l" --libs)
+_apu_invoke(APU_LIBTOOL   "(^| )-L" --link-libtool)
+_apu_invoke(APU_LIBS      ""        --link-ld)
+_apu_invoke(APU_LDFLAGS   ""        --ldflags)
+_apu_invoke(APU_VERSION   ""        --version)
+
+list(GET APU_LIBTOOL 0 APU_LIBTOOL_ARG0)
+
+get_filename_component(APU_LIBTOOL_BASE ${APU_LIBTOOL_ARG0} PATH ) 
+FIND_LIBRARY(APU_LIBRARY NAMES apr-accre-1 aprutil-ACCRE-1 PATHS ${APU_LIBTOOL_BASE})
+
+# compatibility, allow this CMake module to work with the various CMakeList.txt files
+set(APRUTIL_INCLUDE_DIR "${APU_INCLUDES}")
+set(APRUTIL_LIBRARY "${APU_LIBRARY}")
+
+MESSAGE(STATUS "APU AT ${APU_LIBRARY}")
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(APU DEFAULT_MSG APU_INCLUDES APU_LIBS APU_LIBRARY APU_VERSION)
