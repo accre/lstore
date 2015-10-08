63466f8e2a72b412151156ae70d7db6d41fb8667
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2015-10-08 17:24:30 -0400

    More CMake tweaks
    * Fix static building
    * Remove shared_name.cmake
    * Add executable handling
    * Style cleanups

--- src/toolbox/cmake/LStoreCommon.cmake
+++ src/toolbox/cmake/LStoreCommon.cmake
@@ -1,17 +1,42 @@
+# Function to try and find static versions of shared libraries
+macro(static_name full_name static)
+    get_filename_component(lname ${full_name} NAME_WE)
+    get_filename_component(path_name ${full_name} PATH)
+    if(WIN32)
+        string(SUBSTRING ${lname} 3 -1 bname)
+        set(file_name "${bname}.a")
+    elseif(APPLE)
+        set(file_name "${lname}.a")
+    elseif(UNIX)
+        set(file_name "${lname}.a")
+    else()
+        message(SEND_ERROR "Don't know how to mangle the static library name for platform")
+        set(file_name "ERROR")
+    endif()
+    if(EXISTS "${path_name}/${file_name}")
+        set(${static} "${path_name}/${file_name}")
+    else()
+        set(${static} ${full_name})
+    endif()
+endmacro(static_name)
+
 include(FeatureSummary)
 include(CheckIncludeFile)
 include(CMakeDependentOption)
 include(${CMAKE_SOURCE_DIR}/cmake/Date.cmake)
 include(${CMAKE_SOURCE_DIR}/cmake/CompilerVersion.cmake)
 include(${CMAKE_SOURCE_DIR}/cmake/CompilerFlags.cmake)
-include(${CMAKE_SOURCE_DIR}/cmake/shared_name.cmake)
 
 # Accept config options
-option(WANT_RELEASE "Set options for package building, overriding all others" 0)
-option(PROJECT_VERSION "Version string for build" "local")
-CMAKE_DEPENDENT_OPTION(WANT_STATIC "Attempt to build and link statically" TRUE
-                                    "NOT WANT_RELEASE" FALSE)
-
+set(PROJECT_VERSION "local" CACHE STRING "Version string for build")
+set(PROJECT_REVISION "1" CACHE STRING "Revision number for packaging")
+option(WANT_PACKAGE "Set options for package building, overriding all others"
+                    OFF)
+cmake_dependent_option(WANT_STATIC "Attempt to build and link statically" TRUE
+                                   "NOT WANT_PACKAGE" FALSE)
+if(WANT_PACKAGE)
+    set(CMAKE_BUILD_TYPE "Release")
+endif(WANT_PACKAGE)
 
 # Set preprocessor flags.
 # TODO: This should be probably modified if we build on something different
@@ -30,10 +55,10 @@ find_package(APR-ACCRE REQUIRED)
 find_package(APRUtil-ACCRE REQUIRED)
 find_package(Zlib REQUIRED)
 
-include_directories(${OPENSSL_INCLUDE_DIR} ${APR_INCLUDE_DIR} ${APRUTIL_INCLUDE_DIR})
-SET(LIBS ${LIBS} ${OPENSSL_LIBRARIES} ${CRYPTO_LIBRARIES} ${APR_LIBRARY} ${APRUTIL_LIBRARY})
-
-list(APPEND LIBS ${ZLIB_LIBRARY} pthread m dl)
+include_directories(${OPENSSL_INCLUDE_DIR} ${APR_INCLUDE_DIR}
+                    ${APRUTIL_INCLUDE_DIR})
+list(APPEND LIBS ${OPENSSL_LIBRARIES} ${CRYPTO_LIBRARIES} ${APR_LIBRARY}
+                 ${APRUTIL_LIBRARY} ${ZLIB_LIBRARY} pthread m dl)
 if(NOT APPLE)
     # OSX doesn't have/need librt
     list(APPEND LIBS rt)
@@ -62,20 +87,13 @@ else()
     SET(library_lib library)
 endif(WANT_STATIC)
 
-set(sodeps)
-set(lpdeps)
+set(STATIC_LIBS)
 foreach(lib ${LIBS})
-    shared_name(${lib} soname)
-    set(sodeps ${sodeps} ${soname})
-
-    get_filename_component(ldir ${lib} PATH)
-    if (ldir)
-        set(lpdeps ${lpdeps} "-L${ldir}")
-    endif()
+    static_name(${lib} soname)
+    list(APPEND STATIC_LIBS "${soname}")
 endforeach()
 
-target_link_libraries(library LINK_PUBLIC ${lpdeps} ${sodeps})
-
+target_link_libraries(library LINK_PUBLIC ${LIBS})
 install(TARGETS library DESTINATION lib)
 install(FILES ${LSTORE_PROJECT_INCLUDES} DESTINATION include/${LSTORE_PROJECT_NAME}
         COMPONENT devel)
@@ -83,14 +101,27 @@ if(WANT_STATIC)
     install(TARGETS library-static DESTINATION lib COMPONENT devel)
 endif(WANT_STATIC)
 
+#Add the exe build rules
+foreach (f ${LSTORE_EXECUTABLES})
+    add_executable(${f} ${f}.c)
+    set_target_properties(${f} PROPERTIES LINKER_LANGUAGE CXX)
+    if(WANT_STATIC)
+        target_link_libraries(${f} ${library_lib} ${STATIC_LIBS})
+    else()
+        target_link_libraries(${f} ${library_lib})
+    endif(WANT_STATIC)
+endforeach(f)
+
 # Below is used for building packages
 set(CPACK_PACKAGE_NAME "accre-${LSTORE_PROJECT_NAME}")
-set(CPACK_PACKAGE_VERSION ${PROJECT_VERSION})
-set(CPACK_GENERATOR "RPM")
-set(CPACK_PACKAGE_RELEASE 1)
+set(CPACK_PACKAGE_VERSION "${PROJECT_VERSION}")
+set(CPACK_GENERATOR "RPM;DEB")
+set(CPACK_SOURCE_GENERATOR "RPM;DEB")
+set(CPACK_PACKAGE_RELEASE ${PROJECT_REVISION})
 set(CPACK_PACKAGE_CONTACT "Andrew Melo or Alan Tackett")
 set(CPACK_PACKAGE_VENDOR "Advanced Computing Center for Research and Education, Vanderbilt University")
 set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}-${CPACK_PACKAGE_RELEASE}.${CMAKE_SYSTEM_PROCESSOR}")
+set(CPACK_SOURCE_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}-${CPACK_PACKAGE_RELEASE}.${CMAKE_SYSTEM_PROCESSOR}.source")
 
 # Component configuration - currently broken. :(
 set(CPACK_RPM_COMPONENT_INSTALL OFF)
@@ -99,7 +130,10 @@ set(CPACK_ARCHIVE_COMPONENT_INSTALL OFF)
 
 # Generator specific config
 set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON)
-include(CPack)
+
+if(WANT_PACKAGE)
+    include(CPack)
+endif(WANT_PACKAGE)
 
 # Give the summary
 feature_summary(WHAT ALL)
