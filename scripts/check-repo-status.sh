a9173543909d0fb48764348249b1e9eda8038cc0
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2015-10-12 00:31:52 -0500

    Add repo status helper

---
+++ scripts/check-repo-status.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Helper to see status of all subproject repositories
+
+set -eu 
+ABSOLUTE_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
+source $ABSOLUTE_PATH/functions.sh
+statuses=( )
+cd $LSTORE_RELEASE_BASE
+statuses+=("lstore-release $(get_repo_status .)")
+cd source
+for REPO in apr-accre apr-util-accre jerasure toolbox gop ibp lio; do
+    RET="$(get_repo_status $REPO)"
+    GIT=${RET% *}
+    CLEAN=${RET##* }
+    statuses+=("$REPO $GIT $CLEAN")
+done
+
+for STATUS in "${statuses[@]}"; do
+    echo $STATUS
+done
