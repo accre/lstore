3a8da82d3bb89a43d89fd9f88d7d7c4c36b8950a
Matt Heller <matthew.f.heller@accre.vanderbilt.edu>
2015-10-29 06:23:19 -0400

    Create tarballs dir if needed
    Create tarballs dir if needed and update package build completion
    message.

--- scripts/package-internal.sh
+++ scripts/package-internal.sh
@@ -99,4 +99,4 @@ for PACKAGE in apr-accre apr-util-accre jerasure czmq toolbox gop ibp lio; do
     popd 
 done
 
-note "Done! The new packages can be found in ./repo/$PACKAGE_SUBDIR"
+note "Done! The new packages can be found in ./package/$PACKAGE_SUBDIR"
