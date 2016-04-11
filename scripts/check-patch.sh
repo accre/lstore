f07e36ef38848c281ab3d48123c333eff9dc60a5
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2016-04-11 18:07:11 -0500

    Change checkpatch limit
    Tighten checkpatch limits to keep old references out of repository

--- scripts/check-patch.sh
+++ scripts/check-patch.sh
@@ -10,10 +10,19 @@ PATCH_OK=0
 IFS=$'\n';
 
 # list all objects including their size, sort by size, take top 10
-objects=`git verify-pack -v .git/objects/pack/pack-*.idx | grep -v chain | sort -k3nr | head -n 50`
+objects=`git verify-pack -v .git/objects/pack/pack-*.idx | grep -v chain | sort -k3nr | head -n 30`
 
-output="size,pack,SHA,location"
-allObjects=`git rev-list --all --objects`
+# Big blobs to ignore -- from externals
+IGNORE_HASHES="58346973a5f2b4894cb8cd3fc95702ef1b49f882
+               030e77766ab1ac10f4eaebb8786b7604e76b7d75
+               44e0ecff11e3a16ca7656be45268d0bd597afc51
+               63ae69dc6fecaf83c52fba2ad334f4b1369fb1cd
+               1d7024e733f9360ae6b71cc48c2d461e5541744d
+               2873e1cb3ef751c4c620df02cf4e49e2ca3f5105
+               8ab7eab67fe3b40e3cae2ba658f01e40e5bde6b6"
+
+output="size pack SHA location"
+allObjects=`git rev-list --objects HEAD`
 for y in $objects
 do
     # extract the size in bytes
@@ -22,17 +31,24 @@ do
     compressedSize=$((`echo $y | cut -f 6 -d ' '`/1024))
     # extract the SHA
     sha=`echo $y | cut -f 1 -d ' '`
+    # Ignore old giant ref
+    if [[ "$IGNORE_HASHES" =~ "$sha" ]]; then
+        continue
+    fi
     # find the objects location in the repository tree
     other=$(echo "${allObjects}" | grep "$sha")
-    #lineBreak=`echo -e "\n"`
-    if [[ "$size" -gt 811 || "$compressedSize" -gt 251 ]]; then
-        output="${output}\n${size},${compressedSize},${other}"
+    if [ -z "$other" ]; then
+        continue
+    fi
+    # Numbers stolen from initial commit
+    if [[ "$size" -gt 175 || "$compressedSize" -gt 100 ]]; then
+        output="${output}\n${size} ${compressedSize} ${other}"
         PATCH_OK=1
     fi
 done
 
 if [ $PATCH_OK -ne 0 ]; then
     echo "Error! The following objects are too large"
-    echo -e $output | column -t -s ', '
+    echo -e $output
     exit 1
 fi
