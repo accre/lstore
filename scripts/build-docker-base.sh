d581089d5ec35c7754ea834b20fa44e60ed9e4fb
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2016-04-15 01:16:23 -0500

    Update docker configuration
    Restructure docker paths to line up with image name, add in a new
    buildslave image to support docker cloud

--- scripts/build-docker-base.sh
+++ scripts/build-docker-base.sh
@@ -9,34 +9,40 @@ source $ABSOLUTE_PATH/functions.sh
 note "Generating configs"
 $LSTORE_SCRIPT_BASE/generate-docker-base.sh
 
-cd $LSTORE_SCRIPT_BASE/docker/base
+DIRECTORY_NAMES="builder buildslave"
 STATUS=""
+for DIR in $DIRECTORY_NAMES; do
+    cd $LSTORE_SCRIPT_BASE/docker/$DIR
 
-# Parse comand line
-DISTROS=( "$@" )
-if [ ${#DISTROS[@]} -eq 0 ]; then
-    DISTROS=( */ )
-fi
-DISTROS=( "${DISTROS[@]%/}" )
-SUCCESS=0
+    # Parse comand line
+    DISTROS=( "$@" )
+    if [ ${#DISTROS[@]} -eq 0 ]; then
+        DISTROS=( */ )
+    fi
+    DISTROS=( "${DISTROS[@]%/}" )
+    SUCCESS=0
 
-for DISTRO in "${DISTROS[@]}"; do
-    note "Processing $(basename $DISTRO)"
-    docker build --force-rm=true --rm=true \
-        -t "lstore/builder:$(basename $DISTRO)" "$DISTRO"
-    if [ $? -ne 0 ]; then
-        SUCCESS=1
-        STATUS="${STATUS}"$'\n'"Failed to build $DISTRO"
+    for DISTRO in "${DISTROS[@]}"; do
+        if [ ! -d $DISTRO ]; then
+            continue
+        fi
+        note "Processing $(basename $DISTRO)"
+        docker build --force-rm=true --rm=true \
+            -t "lstore/$DIR:$(basename $DISTRO)" "$DISTRO"
+        if [ $? -ne 0 ]; then
+            SUCCESS=1
+            STATUS="${STATUS}"$'\n'"Failed to build $DISTRO"
+        else
+            STATUS="${STATUS}"$'\n'"Successfully built $DISTRO"
+        fi
+    done
+    if [ ! -z "$STATUS" ]; then
+        if [ $SUCCESS -eq 0 ]; then
+            note "$STATUS"
+        else
+            fatal "$STATUS"
+        fi
     else
-        STATUS="${STATUS}"$'\n'"Successfully built $DISTRO"
+        fatal "Nothing was built?"
     fi
 done
-if [ ! -z "$STATUS" ]; then
-    if [ $SUCCESS -eq 0 ]; then
-        note "$STATUS"
-    else
-        fatal "$STATUS"
-    fi
-else
-    fatal "Nothing was built?"
-fi
