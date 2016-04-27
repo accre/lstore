bd5f1f9b40be9552f5154d2f2e1db63bfc7d0327
Matt Heller <matthew.f.heller@accre.vanderbilt.edu>
2016-04-27 15:48:07 -0400

    dpkg-buildpackage support
    Several intertwined changes:
    * Added a top level ./debian folder for packaging a monolithic LStore package
    * Added support for versioning the shared libraries
    * Added pkgconfig metadata
    * Added support for -dbgsym (aka ddeb) package on Debian/Ubuntu/...
    * Added support for -dev package on Debian/Ubuntu/...
    .
    Notes:
    - kept the *.ex sample templates in ./debian, fine if the maintainer wants to rm
    - This supports dpkg-buildpackage calling cmake,
      this does not use the CPack stuff in LStorePackaging.cmake
    - Run "dpkg-buildpackage -us -uc" in the project folder and debs should appear in
      the projects parent folder after a few minutes of building

---
+++ debian/lstore.default.ex
@@ -0,0 +1,10 @@
+# Defaults for lstore initscript
+# sourced by /etc/init.d/lstore
+# installed at /etc/default/lstore by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
