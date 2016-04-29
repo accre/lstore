6480c8cfcbc91a17c46b2aada1d424338811925f
Matthew Heller <matthew.f.heller@gmail.com>
2016-04-29 13:06:48 -0400

    Update Debian/CZMQ packaging (#40)
    Get Debian and CZMQ to build properly

--- scripts/update-repo-internal.sh
+++ scripts/update-repo-internal.sh
@@ -48,7 +48,9 @@ case $PARENT in
         ;;
     ubuntu|debian)
         find build/package/$DISTRO/ -name '*.deb' | \
-            xargs -I{} cp {} build/repo/$PARENT/$RELEASE/packages
+            xargs -I{} cp -u -v {} build/repo/$PARENT/$RELEASE/packages
+            # Since the deb file names don't have extra git versioning appended filenames
+            # may not be unique, 'cp -u' means we copy the newest version.
         pushd build/repo/$PARENT/$RELEASE/packages
         dpkg-scanpackages ./ | gzip >Packages.gz
         popd
