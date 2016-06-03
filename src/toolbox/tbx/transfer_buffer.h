55160e984a215b8f108c31541437cd5494b826de
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2016-06-03 16:05:37 -0500

    rename transfer buffer

--- src/toolbox/tbx/transfer_buffer.h
+++ src/toolbox/tbx/transfer_buffer.h
@@ -37,31 +37,29 @@ typedef struct tbx_tbuf_t tbx_tbuf_t;
 typedef struct tbx_tbuf_var_t tbx_tbuf_var_t;
 
 // Functions
-TBX_API int tbx_tbuf_copy(tbx_tbuf_t *tb_s, size_t off_s, tbx_tbuf_t *tb_d, size_t off_d, size_t nbytes, int blank_missing);
-
-TBX_API void tbx_tbuf_fn(tbx_tbuf_t *tb, size_t total_bytes, void *arg, int (*next_block)(tbx_tbuf_t *tb, size_t pos, tbx_tbuf_var_t *tbv));
-
-TBX_API int tbx_tbuf_memset(tbx_tbuf_t *tb, size_t off, int c, size_t nbytes);
-
-TBX_API void tbx_tbuf_single(tbx_tbuf_t *tb, size_t nbytes, char *buffer);
-
-TBX_API size_t tbx_tbuf_size(tbx_tbuf_t *tb);
-
-TBX_API void tbx_tbuf_vec(tbx_tbuf_t *tb, size_t total_bytes, size_t n_vec, tbx_iovec_t *iov);
-
-TBX_API int tbx_tbuf_test();
-
-TBX_API size_t tbx_tbuf_var_size();
-
-TBX_API int tbx_tbuf_next_block(tbx_tbuf_t *tb, size_t off, tbx_tbuf_var_t *tbv);
-
 TBX_API tbx_iovec_t * tbx_tbuf_var_buffer_get(tbx_tbuf_var_t *tbv);
-
-TBX_API int tbx_tbuf_var_n_iov_get(tbx_tbuf_var_t *tbv);
-
-TBX_API void tbx_tbuf_var_nbytes_set(tbx_tbuf_var_t *tbv, size_t nbytes);
-
+TBX_API int tbx_tbuf_copy(tbx_tbuf_t *tb_s, size_t off_s, tbx_tbuf_t *tb_d,
+                            size_t off_d, size_t nbytes, int blank_missing);
+TBX_API void tbx_tbuf_fn(tbx_tbuf_t *tb, size_t total_bytes, void *arg,
+                            int (*next_block)(tbx_tbuf_t *tb,
+                                                size_t pos,
+                                                tbx_tbuf_var_t *tbv));
+TBX_API int tbx_tbuf_memset(tbx_tbuf_t *tb, size_t off, int c, size_t nbytes);
 TBX_API int tbx_tbuf_next(tbx_tbuf_t *tb, size_t off, tbx_tbuf_var_t *tbv);
+TBX_API int tbx_tbuf_next_block(tbx_tbuf_t *tb,
+                                size_t off,
+                                tbx_tbuf_var_t *tbv);
+TBX_API void tbx_tbuf_single(tbx_tbuf_t *tb, size_t nbytes, char *buffer);
+TBX_API size_t tbx_tbuf_size(tbx_tbuf_t *tb);
+TBX_API int tbx_tbuf_test();
+TBX_API int tbx_tbuf_var_n_iov_get(tbx_tbuf_var_t *tbv);
+TBX_API void tbx_tbuf_var_nbytes_set(tbx_tbuf_var_t *tbv, size_t nbytes);
+TBX_API size_t tbx_tbuf_var_size();
+TBX_API void tbx_tbuf_vec(tbx_tbuf_t *tb,
+                            size_t total_bytes,
+                            size_t n_vec,
+                            tbx_iovec_t *iov);
+
 // Preprocessor macros
 #define TBUFFER_OK 1
 #define TBUFFER_OUTOFSPACE 2
