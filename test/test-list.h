296ec9cb926ac0813e9c1800f19c2a912c97fc6d
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2016-06-06 20:43:00 -0500

    Fix potential string token array bounds accesses
    Clang reported that tbx_stk_escape_text can access past the end of an
    array. It appears it's because clang can't divine out the relationship
    between escape_count and the number of times j gets incremented. Add in
    some asserts to tell clang the access is in-bounds.

--- test/test-list.h
+++ test/test-list.h
@@ -1,7 +1,9 @@
 TEST_DECLARE(always_win)
 
 TEST_DECLARE(tb_stack)
+TEST_DECLARE(tb_stk_escape_text)
 TASK_LIST_START
     TEST_ENTRY(always_win)
     TEST_ENTRY(tb_stack)
+    TEST_ENTRY(tb_stk_escape_text)
 TASK_LIST_END
---
+++ test/test-tb-stk.c
@@ -0,0 +1,34 @@
+/*
+   Copyright 2016 Vanderbilt University
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+#include "task.h"
+#include <tbx/string_token.h>
+#include <string.h>
+
+TEST_IMPL(tb_stk_escape_text) {
+    char *ret;
+    ASSERT(strlen(ret = tbx_stk_escape_text("a", '$', "1")) == 1);
+    NOTE(ret);
+    ASSERT(strlen(ret = tbx_stk_escape_text("a", '$', "a")) == 2);
+    NOTE(ret);
+    ASSERT(strlen(ret = tbx_stk_escape_text("a", '$', "$")) == 2);
+    NOTE(ret);
+    ASSERT(strlen(ret = tbx_stk_escape_text("a", '$', "$a")) == 4);
+    NOTE(ret);
+    ASSERT(strlen(ret = tbx_stk_escape_text("a", '$', "a$")) == 4);
+    NOTE(ret);
+
+    return 0;
+}
