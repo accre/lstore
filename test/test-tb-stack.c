6213a4b68b9e69c0bf76fee5ded542c9bfe64186
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2016-08-07 11:09:37 -0500

    Add more stack tests

--- test/test-tb-stack.c
+++ test/test-tb-stack.c
@@ -1,40 +1,56 @@
 #include "task.h"
 #include <tbx/stack.h>
-#include <stack.h>
 #include <stdio.h>
 
 // Obviously, this should/could be fleshed out more
 TEST_IMPL(tb_stack) {
     tbx_stack_t * stack = tbx_stack_new();
-    NOTE("STACK");
     ASSERT(stack != 0);
-    NOTE("STACK");
     ASSERT(tbx_stack_get_current_ptr(stack) == NULL);
-    NOTE("STACK");
     ASSERT(tbx_stack_get_current_data(stack) == NULL);
-    NOTE("STACK");
     ASSERT(tbx_stack_count(stack) == 0);
-    NOTE("STACK");
+
+    // Add a few elements
+    void *ele1 = (void *) 1;
+    tbx_stack_push(stack, ele1);
+    ASSERT(tbx_stack_count(stack) == 1);
+    ASSERT(tbx_stack_get_current_data(stack) == ele1);
     
+    void *ele2 = (void *) 2;
+    tbx_stack_push(stack, ele2);
+    ASSERT(tbx_stack_count(stack) == 2);
+    ASSERT(tbx_stack_get_current_data(stack) == ele2);
+
+    void *ele3 = (void *) 3;
+    tbx_stack_push(stack, ele3);
+    ASSERT(tbx_stack_count(stack) == 3);
+    ASSERT(tbx_stack_get_current_data(stack) == ele3);
+
+    // Move up and down
+    tbx_stack_move_to_bottom(stack);
+    ASSERT(tbx_stack_get_current_data(stack) == ele1);
+    tbx_stack_move_to_top(stack);
+    ASSERT(tbx_stack_get_current_data(stack) == ele3);
+    tbx_stack_move_down(stack);
+    ASSERT(tbx_stack_get_current_data(stack) == ele2);
+    tbx_stack_move_down(stack);
+    ASSERT(tbx_stack_get_current_data(stack) == ele1);
+    tbx_stack_move_up(stack);
+    ASSERT(tbx_stack_get_current_data(stack) == ele2);
+    tbx_stack_move_up(stack);
+    ASSERT(tbx_stack_get_current_data(stack) == ele3);
+
     tbx_stack_empty(stack, 0);
-    NOTE("STACK");
     ASSERT(stack != 0);
-    NOTE("STACK");
 
     tbx_stack_empty(stack, 1);
-    NOTE("STACK");
     ASSERT(stack != 0);
-    NOTE("STACK");
     
     tbx_stack_t * stack2 = tbx_stack_new();
-    NOTE("STACK");
     ASSERT(stack2 != 0);
-    NOTE("STACK");
 
     tbx_stack_free(stack, 1);
-    NOTE("STACK");
     tbx_stack_free(stack2, 1);
-    NOTE("STACK");
     
     return 0;
 }
