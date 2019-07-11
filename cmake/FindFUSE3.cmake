a9e79834867a102825f837f4bddafaaf67ff7daa
Alan Tackett <alan.tackett@accre.vanderbilt.edu>
2019-07-11 14:47:10 -0500

    Cleanup reporting of FUSE3 package

--- cmake/FindFUSE3.cmake
+++ cmake/FindFUSE3.cmake
@@ -2,7 +2,8 @@
 #
 #  FUSE_INCLUDE_DIR - where to find fuse.h, etc.
 #  FUSE_LIBRARIES   - List of libraries when using FUSE.
-#  FUSE_FOUND       - True if FUSE lib is found.
+#  FUSE3_FOUND      - True if FUSE3 header and libs are found used by CMake 
+#  HAS_FUSE3        - True if FUSE3 found and intended for use by the compiler
 
 # check if already in cache, be silent
 IF (FUSE_INCLUDE_DIR)
@@ -29,19 +30,18 @@ FIND_LIBRARY(FUSE_LIBRARIES
 SET(FUSE_LIBRARY ${FUSE_LIBRARIES})
 
 if (FUSE_LIBRARY AND FUSE_INCLUDE_DIR)
-    SET(FUSE_FOUND 1)
+    SET(FUSE3_FOUND 1)
     SET(HAS_FUSE3 1)
 endif (FUSE_LIBRARY AND FUSE_INCLUDE_DIR)
 
-if (FUSE_FOUND)
-   message(STATUS "Found FUSE3: ${FUSE_LIBRARY} ${FUSE_INCLUDE_DIR}")
-else (FUSE_FOUND)
+if (NOT FUSE3_FOUND)
+#   message(STATUS "Found FUSE3: ${FUSE_LIBRARY} ${FUSE_INCLUDE_DIR}")
+#else
    message(STATUS "Could not find FUSE3 library: LIB=${FUSE_LIBRARY} INC=${FUSE_INCLUDE_DIR}")
-endif (FUSE_FOUND)
+endif()
 
 include ("FindPackageHandleStandardArgs")
-find_package_handle_standard_args ("FUSE" DEFAULT_MSG
+find_package_handle_standard_args ("FUSE3" DEFAULT_MSG
     FUSE_INCLUDE_DIR FUSE_LIBRARIES)
 
 mark_as_advanced (FUSE_INCLUDE_DIR FUSE_LIBRARIES)
-
